ecf31f3f9f1e0381b00133af28bb364a4753648f
Seoyeon <77755620+wjstjdus96@users.noreply.github.com>
2024-03-30 15:25:10 +0900

    [Gold V] Title: Contact, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/1013. Contact/Contact.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+for _ in range(n):
+    s = input()
+    result = True
+
+    while len(s) > 0:
+        if s[:3] == '100':
+            s = s[3:]
+            while s[:1] == "0":
+                s = s[1:]
+            if len(s)==0:
+                result=False
+                break
+            s = s[1:]
+            while s[:1] == "1":
+                if len(s)>=3 and s[:3] == "100":
+                    break
+                else:
+                    s = s[1:]
+        elif s[:2] == "01":
+            s = s[2:]
+        else:
+            result = False
+            break
+    print("YES" if result else "NO")
